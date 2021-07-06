6b588c5cd228bf6aaf9496cf43eea96144cd69e0
Jan <hiasguy@hotmail.com>
2021-07-06 20:10:29 +0200

    JS function draw added

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ function fillShape(id) {
     }
     fields[id] = currentShape;    //setze für das Feld mit der entsprechendenen ID im Array den Wert = currentShape
     console.log(fields);
+    draw();
+}
+
+function draw() {
+    for(let i=0; i < fields.length; i++) {
+        if(fields[i] == 'circle') {                                                 //Wenn das Feld an Stelle i im Array = circle ist, dann
+            document.getElementById('circle-' + i).classList.remove('d-none');      //entferne dem Element mit der id "circle-i" die class d-none
+        }
+
+        if(fields[i] == 'cross') {                                                  //Wenn das Feld an Stelle i im Array = cross ist, dann
+            document.getElementById('cross-' + i).classList.remove('d-none');       //entferne dem Element mit der id "cross-i" die class d-none
+        }               
+    }
 }
