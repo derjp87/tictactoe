080dd91cd4ed9429da59923180d2225dd5935150
Jan <hiasguy@hotmail.com>
2021-07-06 20:53:05 +0200

    JS checkForWin function added

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function fillShape(id) {
     fields[id] = currentShape;    //setze für das Feld mit der entsprechendenen ID im Array den Wert = currentShape
     console.log(fields);
     draw();
+    checkForWin();
 }
 
 function draw() {
@@ -23,4 +24,45 @@ function draw() {
             document.getElementById('cross-' + i).classList.remove('d-none');       //entferne dem Element mit der id "cross-i" die class d-none
         }               
     }
+}
+
+function checkForWin() {
+    let winner;
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {      //Wenn das Feld 0 im Array gleich dem Feld 1 im Array ist UND das Feld 1 im Array gleich dem Feld 2 im Array UND dem Feld 0 im Array ist
+        winner = fields[0];
+    }
+
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (winner) {
+        console.log('GEWONNEN:', winner);
+    }
+    
+
 }
